d474b9b6eb2c215b4eaaadb0f9e3a1e99a9d34fc
Ana-Maria Panaitescu <panaitescuana22@stud.ase.ro>
2025-01-14 23:31:20 +0200

    server updated with the database sync and server start

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
 const cors = require ('cors');
+const config = require('./config');
 
 // Creating the database
 const sequelize = new Sequelize('conference-db', 'user', 'password',{
@@ -12,13 +13,17 @@ const sequelize = new Sequelize('conference-db', 'user', 'password',{
     storage: 'db.sqlite' //Database file
 });
 
-//module.exports = sequelize;
-
 // Import classes
 const User = require('./classes/User');
 const Conference = require('./classes/Conference');
 const Article = require('./classes/Article');
 const Review = require('./classes/Review');
+
+// Import routes
+const userRoutes = require('./routes/userRoute');
+const conferenceRoutes = require('./routes/conferenceRoute');
+const articleRoutes = require('./routes/articleRoute');
+const reviewRoutes = require('./routes/reviewRoute');
 
 // Relationships between tables of the database
 
@@ -38,13 +43,6 @@ Review.belongsTo(Article, { foreignKey: 'articleId' });
 User.hasMany(Review, { foreignKey: 'reviewerId' });
 Review.belongsTo(User, { foreignKey: 'reviewerId' });
 
-
-// Import routes
-const userRoutes = require('./routes/userRoute');
-const conferenceRoutes = require('./routes/conferenceRoute');
-const articleRoutes = require('./routes/articleRoute');
-const reviewRoutes = require('./routes/reviewRoute');
-
 // Middleware configuration
 // Initialize Express app
 const app = express();
@@ -54,25 +52,26 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/conferences', conferenceRoutes);
+app.use('/api/articles', articleRoutes);
+app.use('/api/reviews', reviewRoutes);
+
 // Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ error: 'Something broke!' });
 });
 
-// Routes
-// app.use('/api/users', userRoutes);
-// app.use('/api/conferences', conferenceRoutes);
-// app.use('/api/articles', articleRoutes);
-// app.use('/api/reviews', reviewRoutes);
 
 // Database sync and server start
-// sequelize.sync({ force: false }).then(() => {
-//     app.listen(config.port, () => {
-//         console.log(`Server is running on port ${config.port}`);
-//     });
-// }).catch(err => {
-//     console.error('Unable to connect to the database:', err);
-// });
+sequelize.sync({ force: false }).then(() => {
+    app.listen(config.port, () => {
+        console.log(`Server is running on port ${config.port}`);
+    });
+}).catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
 
-// module.exports = sequelize;
+module.exports = sequelize;
